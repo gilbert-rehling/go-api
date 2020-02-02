0a8f434918fd6a7e7229ba671fcb62d0d5770b6b
gilbert-rehling <gilbert@savv-e.com.au>
2020-02-03 01:11:41 +1100

    Updated branch master

--- api.go
+++ api.go
@@ -1,57 +1,21 @@
 package main
 
-// imports - "regexp" "net/http" "encoding/json" "net"
 import (
-    "time"
-    "database/sql"
-    _ "github.com/go-sql-driver/mysql"
-	"log"
-	"log/syslog"
-	"os"
 	"fmt"
 	"github.com/gilbert-rehling/go-api/models"
 	"github.com/gilbert-rehling/go-api/http"
 )
 
 func init() {
-    // handle some local logging
-    logwriter, err := syslog.New(syslog.LOG_WARNING, "go API running")
+    var text string
 
-    if err == nil {
-        log.SetOutput(logwriter)
-    }
+    text = http.GetPets(db)
 
-    var dir string
-
-    // get the current directory
-    dir, err = os.Getwd()
-    if err != nil {
-        log.Fatal(err)
-    }
-    fmt.Println(dir)
-
-    db, dbErr := sql.Open("mysql", "petapi:petapi2020@tcp(localhost:3306)/petapi")
-
-    if dbErr != nil {
-        log.Panic("DB connection error: %s", dbErr)
-    }
-
-    // To avoid error due to connection being closed in the server
-    db.SetConnMaxLifetime(14000 * time.Second)
-
-    err = db.Ping()
-
-    if err != nil {
-        fmt.Println("ping error:", err)
-        log.Panic("DB ping error: %s", err)
-    }
-
-    http.GetPets(db)
+    fmt.Println(text)
 
 }
 
 func main() {
     // acknowledge the process has started
     fmt.Println("start")
-
 }
