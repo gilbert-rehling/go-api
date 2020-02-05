32c8c6844f1f05b3f6e6e57f80bbc0b6bccdc33e
gilbert-rehling <gilbert@savv-e.com.au>
2020-02-05 16:37:47 +1100

    App now loads required configuration from yaml file

--- src/api.go
+++ src/api.go
@@ -2,36 +2,88 @@ package main
 
 // this router has issues !!  "github.com/julienschmidt/httprouter"
 import (
+    "fmt"
     "time"
 	"log"
+	"os"
 	"net/http"
 	"database/sql"
 	"github.com/julienschmidt/httprouter"
 	"github.com/gilbert-rehling/go-api/db"
     "github.com/gilbert-rehling/go-api/handlers"
+    "gopkg.in/yaml.v2"
 )
 
- //var router *httprouter.Router
+// define the config structure
+type Config struct {
+    Server struct {
+        Host string `yaml:"host"`
+        Port string `yaml:"port"`
+    } `yaml:"server"`
+    Database struct {
+        Type    string `yaml:"type"`
+        Host    string `yaml:"host"`
+        Port    string `yaml:"port"`
+        User    string `yaml:"username"`
+        Pwd     string `yaml:"password"`
+        Name    string `yaml:"name"`
+    } `yaml:"database"`
+}
+var config Config
 
 // initialises the application
-//func init() {
+func init() {
     // init started
 
+    // populate the config from file
+    readFile(&config)
+
+    // view the config data (dev)
+    // fmt.Printf("%+v", config)  // print the config contents
+
     // init finished
-//}
+}
 
+// process any errors
+func processError(err error) {
+    fmt.Println(err)
+    os.Exit(2)
+}
+
+// process configuration from file
+func readFile(config *Config) {
+
+    // get the current directory
+    dir, err := os.Getwd()
+    if err != nil {
+        processError(err)
+    }
+    fmt.Println(dir) // print the current directory
+    var configPath = dir + "/config/db.yaml"
+    fmt.Println(configPath)  // print the path to config
+
+    f, err := os.Open(configPath)
+    if err != nil {
+        processError(err)
+    }
+    defer f.Close()
+
+    decoder := yaml.NewDecoder(f)
+    err = decoder.Decode(config)
+    if err != nil {
+        processError(err)
+    }
+}
+
+// load the main app
 func main() {
-    // main will load the listener
-    // configuration - fixed IP or leave blank
-    var server = "192.168.1.140"
-
-    // configuration - service port !! required !!
-    var port   = "8080"
-
+    // Set up the DB connection
+    // Handle the incoming route requests
+    // Load the listener | server
     var err error
 
     // create the DB link !! todo: using hard coded credentials for testing !!
-    db.Conn, err = sql.Open("mysql", "petapi:petapi2020@tcp(localhost:3306)/petapi")
+    db.Conn, err = sql.Open(config.Database.Type, config.Database.User + ":" + config.Database.Pwd + "@tcp(" + config.Database.Host + ":" + config.Database.Port + ")/" + config.Database.Name)
     if err != nil {
         log.Panic("DB connection error: %s", err)
     }
@@ -64,5 +116,5 @@ func main() {
     router.PUT("/pet/*segment", handlers.PutSwitch)
 
     // set to listen on external facing IP address on DEV box
-    log.Fatal(http.ListenAndServe(server + ":" + port, router))
+    log.Fatal(http.ListenAndServe(config.Server.Host + ":" + config.Server.Port, router))
 }
